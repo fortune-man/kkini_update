c7b2a897dfb45f88c41f1ba5693d21a32776ff98
fortune-man <wnguddl96@knou.ac.kr>
2025-02-12 00:00:12 +0900

    Initial commit

---
+++ src/main/java/kkini/update/KkiniUpdateApplication.java
@@ -0,0 +1,13 @@
+package kkini.update;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkiniUpdateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkiniUpdateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kkini/update/KkiniUpdateApplicationTests.java
@@ -0,0 +1,13 @@
+package kkini.update;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkiniUpdateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
