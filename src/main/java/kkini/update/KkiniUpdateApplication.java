68f840d41a62cb76a81a3c472e31cf32f7d30f48
fortune-man <wnguddl96@knou.ac.kr>
2025-05-28 19:58:34 +0900

    docs : readme update

--- src/main/java/kkini/update/KkiniUpdateApplication.java
+++ src/main/java/kkini/update/KkiniUpdateApplication.java
@@ -8,8 +8,6 @@ public class KkiniUpdateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KkiniUpdateApplication.class, args);
-		System.out.println("hello world!");
-		System.out.println("돈 때문에 아쉬운 소리를 안한다는 건 어떤 느낌일까?");
 	}
 
 }
